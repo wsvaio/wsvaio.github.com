c3bd908b61b026bcc8dc5d61c38f0d6015971a1c
wsvaio <wsvaio@qq.com>
2023-06-15 10:28:58 +0800

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  lang: "zh-CN",
+  title: "WSVAIO",
+  description: "我的个人博客",
+  outDir: "dict",
+  themeConfig: {
+    siteTitle: "WSVAIO",
+    logo: "/logo.jpg",
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "文章", link: "/articles/第一篇文章.md" },
+    ],
+
+    sidebar: [
+      {
+        text: "文章",
+        items: [
+          { text: "第一篇文章", link: "/articles/第一篇文章.md" },
+
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: "github", link: "https://github.com/wsvaio" }],
+
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2023-present WSVAIO",
+    },
+
+    algolia: {
+      appId: "...",
+      apiKey: "...",
+      indexName: "...",
+    },
+
+    // carbonAds: {
+    //   code: "your-carbon-code",
+    //   placement: "your-carbon-placement",
+    // },
+
+    returnToTopLabel: "回到顶部",
+    sidebarMenuLabel: "菜单",
+    darkModeSwitchLabel: "主题",
+  },
+});
---
+++ index.md
@@ -0,0 +1,31 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+title: Home
+
+hero:
+  name: WSVAIO
+  text: 欢迎来到我的博客
+  tagline: 一个热爱分享的程序员
+  image: /logo.jpg
+  actions:
+    - theme: brand
+      text: 开始阅读
+      link: /articles/第一篇文章.md
+    # - theme: alt
+    #   text: 查看
+    #   link: /articles/第一篇文章.md
+
+
+features:
+  - icon: 🌷
+    title: 技术分享
+    details: 分享我在编程和开发中遇到的问题和解决方案
+  - icon: 🌻
+    title: 生活感悟
+    details: 分享我对生活的思考和感悟，以及一些有趣的生活小窍门
+  - icon: 🌼
+    title: 开源和写作
+    details: 学习优秀的开源作品，博采众长，同时将学习到的知识继续开源分享出去
+---
